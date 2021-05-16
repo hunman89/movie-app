7e76409151f5ee4f451ffee74e72c4eb6f4eaf1e
hunman89 <hunman89@gmail.com>
2021-05-16 21:47:29 +0900

    add. props 받고 확인하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
-import Potato from "./Potato";
+import PropTypes from "prop-types";
+
+function Food({ name, rating }) {
+    return (
+        <div>
+            <h3>I like {name}</h3>
+            <h4>{rating}/5.0</h4>
+        </div>
+    );
+}
+
+const foodILike = [
+    { id: 1, rating: 5.0, name: "kimchi" },
+    { id: 2, rating: 4.5, name: "ramen" },
+    { id: 3, rating: 4.2, name: "samgiopsal" },
+    { id: 4, rating: 2.9, name: "chukumi" },
+];
+
+function renderFood(dish) {
+    return <Food key={dish.id} name={dish.name} rating={dish.rating} />;
+}
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+};
 
 function App() {
     return (
         <div className="App">
             <h1>hello</h1>
-            <Potato />
+            {foodILike.map(renderFood)}
         </div>
     );
 }
