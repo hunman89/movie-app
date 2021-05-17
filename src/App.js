e5f38008de2f2c68e96f1c45343d9b652c71457b
hunman89 <hunman89@gmail.com>
2021-05-17 21:12:03 +0900

    add. state, component life cycle 이해

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ name, rating }) {
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  render() {
     return (
-        <div>
-            <h3>I like {name}</h3>
-            <h4>{rating}/5.0</h4>
-        </div>
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
     );
-}
-
-const foodILike = [
-    { id: 1, rating: 5.0, name: "kimchi" },
-    { id: 2, rating: 4.5, name: "ramen" },
-    { id: 3, rating: 4.2, name: "samgiopsal" },
-    { id: 4, rating: 2.9, name: "chukumi" },
-];
-
-function renderFood(dish) {
-    return <Food key={dish.id} name={dish.name} rating={dish.rating} />;
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-};
-
-function App() {
-    return (
-        <div className="App">
-            <h1>hello</h1>
-            {foodILike.map(renderFood)}
-        </div>
-    );
+  }
 }
 
 export default App;
